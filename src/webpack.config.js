28a9f846c955885352901935e4a7d0a9019f54b3
iit-federico-bozzini <federico.bozzini@iit.it>
2017-02-01 17:11:49 +0100

    Added basic assets management with npm and webpack.

---
+++ src/webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/IIT/AllSpeakBundle/Resources/public/js/index.js',
+    output: {
+        path: path.resolve(__dirname, './web/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+            test: /.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015']
+            }
+        },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
+            }
+        ]
+    }
+};
+
