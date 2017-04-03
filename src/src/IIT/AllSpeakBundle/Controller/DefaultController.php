0f6cc231304386fd34641e24dfeb881cdd4487e6
iit-federico-bozzini <federico.bozzini@iit.it>
2017-04-03 14:43:32 +0200

    Added basic survey page.

--- src/src/IIT/AllSpeakBundle/Controller/DefaultController.php
+++ src/src/IIT/AllSpeakBundle/Controller/DefaultController.php
@@ -2,7 +2,13 @@
 
 namespace IIT\AllSpeakBundle\Controller;
 
+use IIT\AllSpeakBundle\Entity\SurveyAnswer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -10,8 +16,88 @@ class DefaultController extends Controller
     {
         return $this->render('IITAllSpeakBundle:Default:index.html.twig');
     }
-    public function surveyAction()
+
+    public function surveyAction(Request $request)
     {
-        return $this->render('IITAllSpeakBundle:Default:survey.html.twig');
+        $surveyAnswer = new SurveyAnswer();
+
+        $sentencesRepository = $this->getDoctrine()->getRepository('IITAllSpeakBundle:SurveySentence');
+
+        $genderChoices = [
+            'Maschio' => 'M',
+            'Femmina' => 'F'
+        ];
+        $surveySentences = $sentencesRepository->findAll();
+        $currentYear = date("Y");
+        $minBirthYear = 1940;
+        $minDiagnosisYear = 1980;
+        $birthYearChoices = range($minBirthYear, $currentYear);
+        $diagnosisYearChoices = range($minDiagnosisYear, $currentYear);
+        $diagnosisChoices = [
+            'A' => 'A',
+            'B' => 'B',
+            'C' => 'C',
+            'D' => 'D',
+        ];
+        $useValueAsLabel = function($value, $key, $index) {return $value;};
+        $form = $this->createFormBuilder($surveyAnswer)
+            ->add('gender', ChoiceType::class, [
+                'label' => 'Genere',
+                'choices' => $genderChoices
+            ])
+            ->add('birthYear', ChoiceType::class, [
+                'label' => 'Anno di nascita',
+                'choices' => $birthYearChoices,
+                'choice_label' => $useValueAsLabel
+            ])
+            ->add('diagnosisYear', ChoiceType::class, [
+                'label' => 'Anno di diagnosi',
+                'choices' => $diagnosisYearChoices,
+                'choice_label' => $useValueAsLabel
+            ])
+            ->add('alsfrsr', NumberType::class, [
+                'label' => 'ALSFR-R'
+            ])
+            ->add('verbalScore', NumberType ::class, [
+                'label' => 'Score verbale'
+            ])
+            ->add('diagnosis', ChoiceType::class, [
+                'label' => 'Diagnosi',
+                'choices' => $diagnosisChoices
+            ])
+            ->add('sentences', ChoiceType::class, [
+                'label' => "Selezionare le frasi piu' importanti",
+                'multiple' => true,
+                'expanded' => true,
+                'choices' => $surveySentences,
+                'choice_label' => 'getText',
+                'choice_value' => 'getId'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Conferma'
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $surveyAnswer = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($surveyAnswer);
+            $em->flush();
+
+            return $this->redirectToRoute('survey_completed');
+        }
+
+        return $this->render('IITAllSpeakBundle:Default:survey.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function surveyCompletedAction(Request $request)
+    {
+        return $this->render('IITAllSpeakBundle:Default:surveyCompleted.html.twig');
     }
 }
