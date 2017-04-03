80419efde3dc635c4af2cd356fead29a60d6f7f5
iit-federico-bozzini <federico.bozzini@iit.it>
2017-04-03 14:38:40 +0200

    Added script for loading initial survey answers.

---
+++ src/src/IIT/AllSpeakBundle/DataFixtures/ORM/LoadSurveySentences.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace IIT\AllSpeakBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use IIT\AllSpeakBundle\Entity\SurveySentence;
+
+class LoadSurveySentences implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $sentences = [
+            'Ho fame',
+            'Ho sete',
+            'Ho sonno',
+            'Ho caldo',
+            'Ho freddo',
+            'Devo andare in bagno',
+            'Ho dolore',
+            'Ciao',
+            'Come stai?',
+            'Come sta...?',
+            'Ci vediamo domani',
+            'Chiama ...',
+            'Salutami ...',
+            'Ti voglio bene',
+            'Ho troppa saliva',
+            'Ho bisogno di lavarmi',
+            'Grazie',
+            'Prego',
+            'Sono arrabbiato',
+            'Sono triste',
+            'Tutto bene',
+            'Passami il telefono',
+            'Voglio stare solo'
+        ];
+        foreach ($sentences as $s) {
+            $surveySentence = new SurveySentence();
+            $surveySentence->setText($s);
+            $manager->persist($surveySentence);
+        };
+
+        $manager->flush();
+    }
+}
