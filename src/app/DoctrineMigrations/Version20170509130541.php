304198dd9db082969388ef23d25d3fb6b43b60d9
iit-federico-bozzini <federico.bozzini@iit.it>
2017-05-09 15:06:19 +0200

    Added migrations.

---
+++ src/app/DoctrineMigrations/Version20170509130541.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170509130541 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE survey_answer ADD diagnosis_date DATETIME DEFAULT NULL, DROP diagnosis_year, CHANGE verbal_score communication_function SMALLINT NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE survey_answer ADD diagnosis_year SMALLINT DEFAULT NULL, DROP diagnosis_date, CHANGE communication_function verbal_score SMALLINT NOT NULL');
+    }
+}
